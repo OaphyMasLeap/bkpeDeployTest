baa7c582c0ff8cd096202f9a6325a41cb98c0818
OaphyMasLeap <oaphy@masleap.io>
2022-07-06 16:27:48 +0600

    updated

--- bkpe/urls.py
+++ bkpe/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,  re_path # re_pathadded for heroku test
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -30,6 +30,7 @@ urlpatterns = [
     path('api/orders/', include('base.urls.order_urls')),
     path('api/vendors/', include('vendor.urls')),
     url(r'^$', 'path.to.function'),
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
